a648af50d5ca176b30870a38bd7e1eff978db028
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2024-04-11 06:33:25 -0300

    additing readme

--- obj/Debug/net7.0/teste.AssemblyInfo.cs
+++ obj/Debug/net7.0/teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd7c83bf879497a9c97357943faf7c6e8ea59bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
